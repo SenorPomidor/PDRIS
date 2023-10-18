29aab42acaab390aa6d3249b0e17da2e34bfd3c7
SenorPomidor <mega.khomenok@mail.ru>
2023-10-19 00:20:07 +0300

    Исправлена ошибка

--- run-images.sh
+++ run-images.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker network create mynetwork
+
 echo "Пулим..."
 
 docker pull senorpomidor/pdris:random-number
@@ -7,7 +9,7 @@ docker pull senorpomidor/pdris:feign-client
 
 echo "Запускаем..."
 
-docker run -d -p 8080:8080 senorpomidor/pdris:random-number
-docker run -d -p 8081:8081 senorpomidor/pdris:feign-client
+docker run -d -p 8080:8080 --name random-number --network=mynetwork senorpomidor/pdris:random-number
+docker run -d -p 8081:8081 --name feign-client --network=mynetwork senorpomidor/pdris:feign-client
 
 echo "Готово!"
